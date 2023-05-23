a14f3cdeb4d0aff7eae7b1b36e09fbbe69579766
leonfernandes <44719892+leonfernandes@users.noreply.github.com>
2023-05-24 01:43:17 +0530

    s3 method model_spec

--- R/fit_splits.R
+++ R/fit_splits.R
@@ -14,7 +14,12 @@
 #'      for goodness-of-fit tests.
 #' @param use_fit_resamples  If `TRUE`, uses [tune::fit_resamples()].
 #' @export
-fit_splits <- function(
+fit_splits <- function(object, ...) {
+    UseMethod("fit_splits")
+}
+
+#' @rdname fit_splits
+fit_splits.workflow <- function(
     object,
     data,
     num_analysis,
@@ -24,7 +29,7 @@ fit_splits <- function(
     metrics = NULL,
     control = tune::control_resamples()
 ) {
-    fn <- fit_splits_impl
+    fn <- fit_splits_impl_workflow
     if (use_fit_resamples) {
         fn <- fit_splits_tune
     }
@@ -36,6 +41,61 @@ fit_splits <- function(
         metrics = metrics,
         control = control
     )
+}
+
+#' @rdname fit_splits
+fit_splits.model_spec <- function(
+    object,
+    formula,
+    data,
+    num_analysis,
+    num_assessment,
+    ...,
+    metrics = NULL,
+    control = tune::control_resamples()
+) {
+    y_nm <- tune::outcome_names(formula)
+    settings_tbl <- sample_splits(data, num_analysis, num_assessment)
+    fitted_tbl <-
+        settings_tbl |>
+        dplyr::group_by(analysis_idx) |>
+        dplyr::summarise(
+            obj_fit = list(
+                generics::fit(
+                    object,
+                    formula,
+                    vctrs::vec_slice(data, 1:analysis_idx[1])
+                )
+            ),
+            obj_resids = list(
+                generics::augment(
+                    obj_fit[[1]],
+                    data
+                ) |>
+                    dplyr::select(.pred, !!rlang::sym(y_nm))
+            )
+        )
+    res <- settings_tbl |>
+        dplyr::left_join(fitted_tbl, by = dplyr::join_by("analysis_idx")) |>
+        dplyr::mutate(
+            .predictions = purrr::map(
+                assessment_idx, ~ vctrs::vec_slice(
+                    obj_resids[[1]],
+                    vctrs::vec_size(obj_resids[[1]]) - .x + 1:.x
+                )
+            )
+        )
+    if (!is.null(metrics)) {
+        res <-
+            res |>
+            dplyr::mutate(
+                .metrics = purrr::map(
+                    .predictions,
+                    ~ metrics(.x, truth = !!rlang::sym(y_nm), estimate = .pred)
+                )
+            )
+    }
+    tibble::new_tibble(res, "smp_spl_results")
 }
 
 fit_splits_tune <- function(
@@ -72,7 +132,7 @@ fit_splits_tune <- function(
         dplyr::left_join(settings_tbl, by = dplyr::join_by("id"))
 }
 
-fit_splits_impl <- function(
+fit_splits_impl_workflow <- function(
     object,
     data,
     num_analysis,
