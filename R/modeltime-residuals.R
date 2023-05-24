127b292ac1d1532d17685201c53a0f1b97e39074
leonfernandes <44719892+leonfernandes@users.noreply.github.com>
2023-05-25 02:36:03 +0530

    s3method mdltime

---
+++ R/modeltime-residuals.R
@@ -0,0 +1,9 @@
+#' Extract fitted residuals from arima models
+#'
+#' @inheritParams forecast::residuals.forecast
+#' @export
+residuals._Arima_fit_impl <- function(
+    object, type = c("innovation", "response", "regression"), h = 1, ...
+) {
+    stats::residuals(object$fit$models$model_1, type, h, ...)
+}
