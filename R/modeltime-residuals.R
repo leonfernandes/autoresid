42db0a35e95decab4e3d5044f7de28c8608a0e36
leonfernandes <44719892+leonfernandes@users.noreply.github.com>
2023-05-26 14:10:38 +0530

    magrittr pipe

--- R/modeltime-residuals.R
+++ R/modeltime-residuals.R
@@ -5,6 +5,6 @@
 residuals._Arima_fit_impl <- function(
     object, type = c("innovation", "response", "regression"), h = 1, ...
 ) {
-    stats::residuals(object$fit$models$model_1, type, h, ...) |>
-        (\(.) tibble::tibble(.resid = .))()
+    stats::residuals(object$fit$models$model_1, type, h, ...) %>%
+        tibble::tibble(.resid = .)
 }
